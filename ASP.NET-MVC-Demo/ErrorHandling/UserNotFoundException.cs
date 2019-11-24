a19b09378924baa59947ff48334ed8f74bf78406
Matt Hurst <matthew@mshurst.co.uk>
2019-11-24 23:44:24 +0000

    Added custom UserNotFoundException used when API does not return any data

---
+++ ASP.NET-MVC-Demo/ErrorHandling/UserNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Demo.ErrorHandling
+{
+    public class UserNotFoundException : Exception
+    {
+        public string Username { get; set; }
+
+        public UserNotFoundException(string username)
+        {
+            Username = username;
+        }
+    }
+}
